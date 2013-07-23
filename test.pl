7d88a82ded491d8a5950b1fc5ec9a7c30f251775
Albert Vilella <avilella@gmail.com>
2013-07-23 13:59:40 +0100

    simple example

--- test.pl
+++ test.pl
@@ -1,26 +1,18 @@
 #!/usr/bin/perl
 use strict;
-use Getopt::Long;
 use Set::IntervalTree;
 use JSON;
-
-my ($inputfile,$debug);
-
-GetOptions(
-	   'i|input|inputfile:s' => \$inputfile,
-           'debug' => \$debug,
-          );
-
-# Printing .json
+# use lib 'libperl/lib/perl5';
 my $json = JSON->new->allow_nonref;
 my $hash;
 $hash->{alfa}{beta} = 1;
 push @{$hash->{alfa}{gamma}}, 2;
 push @{$hash->{alfa}{gamma}}, 3;
-push @{$hash->{alfa}{gamma}}, 4;
 $hash->{delta} = 5;
 my $json_results = $json->encode( $hash );
-
 print "$json_results\n";
+
+my $t = Set::IntervalTree->new;
+$t->insert({true_start=>1,true_end=>99},1,99);
 
 1;
